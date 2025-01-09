b405e9b6fcd636a73f9209e683cd663c93a3cba4
Nithiyanantham <r.nandhanraghu123@gmail.com>
2025-01-09 16:49:47 +0530

    Signature generation

--- src/index.js
+++ src/index.js
@@ -40,8 +40,8 @@ app.post('/', (req, res) => {
   const oHeader = { alg: 'HS256', typ: 'JWT' }
 
   const oPayload = {
-    appKey: process.env.ZOOM_MEETING_SDK_KEY,
-    sdkKey: process.env.ZOOM_MEETING_SDK_KEY,
+    appKey: process.env.ZOOM_MEETING_HOST_KEY,
+    sdkKey: process.env.ZOOM_MEETING_HOST_KEY,
     mn: meetingNumber,
     role,
     iat,
@@ -51,8 +51,56 @@ app.post('/', (req, res) => {
 
   const sHeader = JSON.stringify(oHeader)
   const sPayload = JSON.stringify(oPayload)
-  const sdkJWT = KJUR.jws.JWS.sign('HS256', sHeader, sPayload, process.env.ZOOM_MEETING_SDK_SECRET)
+  const sdkJWT = KJUR.jws.JWS.sign('HS256', sHeader, sPayload, process.env.ZOOM_MEETING_HOST_SECRET)
   return res.json({ signature: sdkJWT })
+})
+
+app.get('/get-access-token', async (req, res) => {
+  try {
+    const body = new URLSearchParams({
+      grant_type: 'account_credentials',
+      account_id: process.env.ZOOM_MEETING_ACCOUNT_ID,
+      client_id: process.env.ZOOM_MEETING_HOST_KEY,
+      client_secret: process.env.ZOOM_MEETING_HOST_SECRET
+    }).toString()
+    const response = await fetch('https://zoom.us/oauth/token', {
+      method: 'POST',
+      body: body,
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    })
+
+    if (!response.ok) {
+      return res.status(response.status).json({ error: 'Failed to fetch access token', details: await response.text() })
+    }
+
+    const data = await response.json()
+    res.json(data) // Return the access token and other response details
+  } catch (error) {
+    console.error('Error fetching access token:', error)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
+
+app.post('/zakToken', async (req, res) => {
+  try {
+    const response = await fetch('https://api.zoom.us/v2/users/me/token?type=zak', {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${req.body.accesstoken}`
+      }
+    })
+
+    if (!response.ok) {
+      return res.status(response.status).json({ error: 'Failed to fetch Zak token', details: await response.text() })
+    }
+
+    const data = await response.json()
+    res.json(data) // Return the access token and other response details
+  } catch (error) {
+    console.error('Error fetching access token:', error)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
 })
 
 app.listen(port, () => console.log(`Zoom Meeting SDK Auth Endpoint Sample Node.js, listening on port ${port}!`))
